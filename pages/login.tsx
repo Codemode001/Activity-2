fe812ef98e3b9d21c8f1ecc772d52a3d61fcd24a
Harold <haroldrex12331@gmail.com>
2023-08-01 10:23:38 +0800

    more dependencies

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,8 @@ const Login = () => {
   return (
     <Main>
       <Container>
-        <Header>login</Header>
+        <Header>LOGIN</Header>
+        <Body></Body>
       </Container>
     </Main>
   );
@@ -14,12 +15,19 @@ const Login = () => {
 
 export default Login;
 
+const Body = styled.div`
+  width: 90%;
+`;
+
 const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 2rem;
   font-weight: 600;
+  height: 15%;
+  border-bottom: 1px solid gray;
+  width: 90%;
 `;
 
 const Container = styled.div`
@@ -27,6 +35,10 @@ const Container = styled.div`
   width: 40%;
   height: 60%;
   border-radius: 10px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Main = styled.div`
